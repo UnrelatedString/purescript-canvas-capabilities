ee5911b6c75b62858cc2c4ee26cb4c62d6537c72
UnrelatedString <unrelatedstring@gmail.com>
2025-04-12 23:14:55 -0400

    fuck forgot I left soup in the microwave like 10 minutes ago damn you gacha cookie clicker

--- src/Graphics/Canvas/Class/CanvasPath.js
+++ src/Graphics/Canvas/Class/CanvasPath.js
@@ -1,36 +1,36 @@
-export function closePath_() {
-
+export function closePath_(ctx) {
+    ctx.closePath();
 }
 
-export function moveTo_() {
-
+export function moveTo_(ctx, p) {
+    ctx.moveTo(p.x, p.y);
 }
 
-export function lineTo_() {
-
+export function lineTo_(ctx, p) {
+    ctx.lineTo(p.x, p.y);
 }
 
-export function quadraticCurveTo_() {
-
+export function quadraticCurveTo_(ctx, c, p) {
+    ctx.quadraticCurveTo(c.x, c.y, p.x, p.y);
 }
 
-export function bezierCurveTo_() {
-
+export function bezierCurveTo_(ctx, c1, c2, p) {
+    ctx.bezierCurveTo(c1.x, c1.y, c2.x, c2.y, p.x, p.y);
 }
 
-export function arcTo_() {
-
+export function arcTo_(ctx, c1, c2, r) {
+    ctx.arcTo(c1.x, c1.y, c2.x, c2.y, r);
 }
 
-export function rect_() {
-
+export function rect_(ctx, r) {
+    ctx.rect(r.x, r.y, r.w, r.h);
 }
 
-export function roundRect_() {
-
+export function roundRect_(ctx, rc, rds) {
+    cts.roundRect(rc.x, rc.y, rc.w, rc.h, rds);
 }
 
-export function arc_() {
+export function arc_(ctx, p, ) {
 
 }
 
